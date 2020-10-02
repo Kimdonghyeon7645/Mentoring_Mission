0acd6323b4aed449a604ee74cb4203250c344f9b
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2020-10-02 23:56:50 +0900

    code : make program run on the console

--- 게시판 만들기.py
+++ 게시판 만들기.py
@@ -1,3 +1,6 @@
+import sys
+
+
 class Post:
     post_list = []
 
@@ -6,25 +9,30 @@ class Post:
         self.body = body
 
     @classmethod
-    def add_post(cls, title, body):
-        if type(title) == type(body) == type('i am str'):
-            new_post = cls(title, body)
-            cls.post_list.append(new_post)
-        else:
-            print("제목과 본문은 문자열이여야 합니다!")
+    def add_post(cls):
+        title = input("추가할 글의 제목을 입력 : ").rstrip()
+        body = input("추가할 글의 본문을 입력 : ").rstrip()
+        if not (title and body):
+            print("[!] 제목과 본문은 공백일 수 없습니다!")
+        if not (type(title) == type(body) == type('str')):
+            print("[!] 제목과 본문은 문자열이여야 합니다!")
+
+        new_post = cls(title, body)
+        cls.post_list.append(new_post)
 
     @classmethod
-    def read_post_list(cls, read_and_print=False):
-        if read_and_print:
-            print("\n- 출력 결과 -")
-            for p in cls.post_list:
-                print(f'제목 - {p.title}', f'- 본문 :{p.body}\n', sep='\n')
-        else:
-            return cls.post_list
+    def read_post_list(cls):
+        print("\n- 출력 결과 -")
+        for p in cls.post_list:
+            print(f'- 제목: {p.title}', f'본문: {p.body}\n', sep='\n')
 
     @classmethod
-    def delete_post(cls, is_title='', is_body=''):
-        for target in cls.find_post(is_title, is_body):
+    def delete_post(cls):
+        title = input("삭제할 글의 제목을 입력 (모든 제목 선택시 공백 입력) : ").rstrip()
+        body = input("삭제할 글의 본문을 입력 (모든 본문 선택시 공백 입력) : ").rstrip()
+        if not (type(title) == type(body) == type('str')):
+            print("[!] 제목과 본문은 문자열이여야 합니다!")
+        for target in cls.find_post(title, body):
             cls.post_list.remove(target)
 
     @classmethod
@@ -39,35 +47,28 @@ class Post:
             print("\n- 검색결과 -")
             for i, p in enumerate(li):
                 print(f'{i}번째 post, 제목 : {p.title}, 본문 : {p.body}')
-            index = int(input("<!> 검색된 결과가 여러개 입니다. 선택할 요소의 인덱스를 입력하세요\n모두 선택시 a를 입력 : ").rstrip())
+            index = int(input("[!] 검색된 결과가 여러개 입니다. 선택할 요소의 인덱스를 입력하세요\n(모두 선택시 문자를 입력) : ").rstrip())
             if 0 <= index < len(li):
                 li = [li[index]]
             else:
-                print("불가능한 인덱스입니다! 모두 선택한 상태로 유지합니다...")
+                print("[!] 불가능한 인덱스입니다! 모두 선택한 상태로 유지합니다...")
         return li
 
+    @classmethod
+    def exe_command(cls, num):
+        commands = {'1': 'add_post', '2': 'read_post_list', '3': 'delete_post'}
+        if num == '4':
+            print("...프로그램을 종료합니다...")
+            sys.exit(0)
+        if num not in commands.keys():
+            print("[!] 존재하지 않는 명령 입니다!\n")
+            return -1
+        exec(f"cls.{commands[num]}()")
 
-# TODO 아래 부분 부터는 테스트 코드입니다
+
 if __name__ == '__main__':
-    # 첫 post 작성 & 출력
-    Post.add_post('파이썬 vs 자바', '파이썬을 사랑하지만 내가 가고싶은 회사는 자바를 사랑한다. 회사인가 사랑인가?')
-    Post.read_post_list(read_and_print=True)
-
-    # 여러개 post 작성 & 출력
-    write_list = [('의문점', '혹시 내가 기초가 부족하여서 이것을 하고 있는가?'),
-                  ('늦잠', '일어나보니 12시다. 앞으로도 밤의 시간을 보낼까 아님 아침의 시간을 보낼까?')]
-    for param in write_list:
-        Post.add_post(param[0], param[1])
-    Post.read_post_list(read_and_print=True)
-
-    # 제목이 중복되는 post 작성 & 출력
-    Post.add_post('늦잠', '좋은 아침인데 문명이 생각난다. 오늘은 이제 안녕인 것 같다.')
-    Post.read_post_list(read_and_print=True)
-
-    # 중복되지 않는 post 삭제 & 출력
-    Post.delete_post(is_title='의문점')
-    Post.read_post_list(read_and_print=True)
-
-    # 제목이 중복되는 post 삭제 & 출력
-    Post.delete_post(is_title='늦잠')
-    Post.read_post_list(read_and_print=True)
+    print("-- 게시판 관리 콘솔 어플리케이션이 정상 실행되었습니다 ^^7 --\n")
+    while True:
+        print("어떤 명령을 실행하시겠습니까? \n(1.글 추가  2.글목록 조회  3.글 삭제  4.프로그램 종료)")
+        choice = input("(원하는 명령의 숫자를 입력하세요) : ").strip()
+        Post.exe_command(choice)
