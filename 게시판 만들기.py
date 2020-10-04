17ebe0832cca5d30dfcf3052c6f7c9e5747cc234
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2020-10-04 15:31:18 +0900

    code : apply comment

--- 게시판 만들기.py
+++ 게시판 만들기.py
@@ -1,74 +1,55 @@
-import sys
-
-
 class Post:
-    post_list = []
-
     def __init__(self, title, body):
         self.title = title
         self.body = body
 
-    @classmethod
-    def add_post(cls):
-        title = input("추가할 글의 제목을 입력 : ").rstrip()
-        body = input("추가할 글의 본문을 입력 : ").rstrip()
+
+class Service:
+    def __init__(self):
+        self.post_list = []
+
+    def add_post(self, title, body):
         if not (title and body):
             print("[!] 제목과 본문은 공백일 수 없습니다!")
-        if not (type(title) == type(body) == type('str')):
-            print("[!] 제목과 본문은 문자열이여야 합니다!")
 
-        new_post = cls(title, body)
-        cls.post_list.append(new_post)
+        new_post = Post(title, body)
+        self.post_list.append(new_post)
 
-    @classmethod
-    def read_post_list(cls):
-        print("\n- 출력 결과 -")
-        for p in cls.post_list:
-            print(f'- 제목: {p.title}', f'본문: {p.body}\n', sep='\n')
+    def read_posts(self):
+        print("- 출력 결과 -")
+        self.get_posts()
 
-    @classmethod
-    def delete_post(cls):
-        title = input("삭제할 글의 제목을 입력 (모든 제목 선택시 공백 입력) : ").rstrip()
-        body = input("삭제할 글의 본문을 입력 (모든 본문 선택시 공백 입력) : ").rstrip()
-        if not (type(title) == type(body) == type('str')):
-            print("[!] 제목과 본문은 문자열이여야 합니다!")
-        for target in cls.find_post(title, body):
-            cls.post_list.remove(target)
+    def get_posts(self):
+        for ele in self.post_list:
+            index = self.post_list.index(ele)
+            print(f"{index:>3}번 포스트 \t 제목: {ele.title} / 본문: {ele.body}")
 
-    @classmethod
-    def find_post(cls, title, body):
-        found_list = [p for p in cls.post_list
-                      if (p.title == title or not title) and (p.body == body or not body)]
-        return cls.choice_post(found_list)
+    def delete_post(self, index):
+        target = self.post_list[index]
+        self.post_list.remove(target)
 
-    @staticmethod
-    def choice_post(li):
-        if len(li) > 1:
-            print("\n- 검색결과 -")
-            for i, p in enumerate(li):
-                print(f'{i}번째 post, 제목 : {p.title}, 본문 : {p.body}')
-            index = int(input("[!] 검색된 결과가 여러개 입니다. 선택할 요소의 인덱스를 입력하세요\n(모두 선택시 문자를 입력) : ").rstrip())
-            if 0 <= index < len(li):
-                li = [li[index]]
+    def exe_command(self):
+        while True:
+            print("\n어떤 명령을 실행하시겠습니까? \n(1.글 추가  2.글목록 조회  3.글 삭제  4.프로그램 종료)")
+            num = input("(원하는 명령의 숫자를 입력하세요) : ").strip()
+
+            if num == '1':
+                t, b = input("글의 제목을 입력하세요 : "), input("글의 본문을 입력하세요 : ")
+                self.add_post(t, b)
+            elif num == '2':
+                self.read_posts()
+            elif num == '3':
+                index = int(input("삭제할 글의 인덱스를 입력하세요 : "))
+                self.delete_post(index)
+            elif num == '4':
+                print("\n프로그램을 종료합니다......")
+                break
             else:
-                print("[!] 불가능한 인덱스입니다! 모두 선택한 상태로 유지합니다...")
-        return li
-
-    @classmethod
-    def exe_command(cls, num):
-        commands = {'1': 'add_post', '2': 'read_post_list', '3': 'delete_post'}
-        if num == '4':
-            print("...프로그램을 종료합니다...")
-            sys.exit(0)
-        if num not in commands.keys():
-            print("[!] 존재하지 않는 명령 입니다!\n")
-            return -1
-        exec(f"cls.{commands[num]}()")
+                print("[!] 존재하지 않는 명령 입니다!\n")
+                continue
 
 
 if __name__ == '__main__':
-    print("-- 게시판 관리 콘솔 어플리케이션이 정상 실행되었습니다 ^^7 --\n")
-    while True:
-        print("어떤 명령을 실행하시겠습니까? \n(1.글 추가  2.글목록 조회  3.글 삭제  4.프로그램 종료)")
-        choice = input("(원하는 명령의 숫자를 입력하세요) : ").strip()
-        Post.exe_command(choice)
+    print("-- 게시판 관리 콘솔 어플리케이션이 정상 실행되었습니다 ^^7 --")
+    my_post = Service()
+    my_post.exe_command()
